22373a42a780ef0393aa1fd4389c68167bdf6ae0
Mihaela <mihaela@L-108.local>
2017-04-09 19:41:35 +0200

    final update

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -4,6 +4,10 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import static android.R.attr.data;
+import static android.app.PendingIntent.getActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -21,32 +25,44 @@ public class MainActivity extends AppCompatActivity {
     public void addThreeForTeamA(View v){
         scoreTeamA = scoreTeamA + 3;
         displayForTeamA(scoreTeamA);
+        Toast.makeText(MainActivity.this,
+                "Great!!!", Toast.LENGTH_LONG).show();
     }
 
     public void addTwoForTeamA(View v){
         scoreTeamA = scoreTeamA + 2;
         displayForTeamA(scoreTeamA);
+        Toast.makeText(MainActivity.this,
+                "Almost there!!!", Toast.LENGTH_LONG).show();
     }
 
     public void addOneForTeamA(View v){
         scoreTeamA = scoreTeamA + 1;
         displayForTeamA(scoreTeamA);
+        Toast.makeText(MainActivity.this,
+                "yuhuu!!!", Toast.LENGTH_LONG).show();
     }
 
 
     public void addThreeForTeamB(View v){
         scoreTeamB = scoreTeamB + 3;
         displayForTeamB(scoreTeamB);
+        Toast.makeText(MainActivity.this,
+                "Great!!!", Toast.LENGTH_LONG).show();
     }
 
     public void addTwoForTeamB(View v){
         scoreTeamB = scoreTeamB + 2;
         displayForTeamB(scoreTeamB);
+        Toast.makeText(MainActivity.this,
+                "Almost there!!!", Toast.LENGTH_LONG).show();
     }
 
     public void addOneForTeamB(View v){
         scoreTeamB = scoreTeamB + 1;
         displayForTeamB(scoreTeamB);
+        Toast.makeText(MainActivity.this,
+                "yuhuu!!!", Toast.LENGTH_LONG).show();
     }
     /**
      * Displays the given score for Team A.
@@ -68,5 +84,7 @@ public class MainActivity extends AppCompatActivity {
         scoreTeamB = 0;
         displayForTeamA(scoreTeamA);
         displayForTeamB(scoreTeamB);
+        Toast.makeText(MainActivity.this,
+                "Back to the beginning!", Toast.LENGTH_LONG).show();;
     }
 }
